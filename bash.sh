76807f9cb0874aa90be7631df96ed5fc93b7a9b8
lei.wang <lei.wang@kaixiangtech.com>
2018-09-14 16:03:17 +0800

    this vim plugs about python

---
+++ bash.sh
@@ -0,0 +1,5 @@
+git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
+dpkg -i cscope_15.8b-1build1_amd64.deb
+cp .vimrc /root/
+#find . -name "*.py“ -o -name "*.sh" >cscope.filles
+#cscope -Rbqk
